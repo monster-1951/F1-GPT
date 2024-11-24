b917b86976250a69e37f01a426642bef57fc4f40
monster-1951 <shivashankarsince19@gmail.com>
2024-11-24 20:37:35 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
 import UserInput from "@/components/custom/UserInput";
-import Info from "@/components/custom/Info";
 
 export default function Home() {
   return (
-   <main>
-    <UserInput/>
-   </main>
+    <main>
+      <UserInput />
+    </main>
   );
 }
